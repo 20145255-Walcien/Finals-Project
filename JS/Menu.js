0ab68d7032beb1f93dd30eb66ae9aa92fec54dd3
20145255-Walcien <79896255+20145255-Walcien@users.noreply.github.com>
2021-05-07 19:06:44 +0800

    Delete Menu.js

--- JS/Menu.js
+++
@@ -1,9 +0,0 @@
-//this function lets the menu button drop down the list of navigation buttons
-function menu () {
-    var x = document.getElementById("menu");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
